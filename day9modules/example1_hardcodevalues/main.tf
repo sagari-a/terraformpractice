ab713543bd95b63781360d892125cb4385e28d8a
sagari <qhamsa05@gmail.com>
2024-03-21 21:29:06 +0530

    day9 updates

--- day9modules/example1_hardcodevalues/main.tf
+++ day9modules/example1_hardcodevalues/main.tf
@@ -6,9 +6,9 @@ module "ec2_instance" {
 
     source = "../ec2instance_module" 
 
-      ami_id  	 = "ami-013168dc3850ef002"
-      instance   = "t2.micro"
-      keyname    = "devops"
+      ami_id  	 = "ami-013168dc3850ef002"   # ami_id is linked to the variable amid_id in the main.tf of the ec2instance_module folder
+      instance   = "t2.micro"                # instance is linked to the variable instance in the main.tf of the ec2instance_module folder
+      keyname    = "devops"                  # keyname is linked to the variable keyname in the main.tf of the ec2instance_module folder
 
     
 }
--- day9modules/example2_without_hardcodevalues/main.tf
+++ day9modules/example2_without_hardcodevalues/main.tf
@@ -6,9 +6,9 @@ module "ec2_instance" {
 
     source = "../ec2instance_module" 
 
-      ami_id  	 = var.ami
-      instance   = var.instance_type
-      keyname    = var.key_name
+      ami_id  	 = var.ami              # ami_id is linked to the variable amid_id in the main.tf of the ec2instance_module folder and get its value from var.ami in the variables.tf in this folder
+      instance   = var.instance_type    # instance is linked to the variable instance in the main.tf of the ec2instance_module folder and get its value from var.instance_type in the variables.tf in this folder
+      keyname    = var.key_name         # keyname is linked to the variable keyname in the main.tf of the ec2instance_module folder and get its value from var.key_name in the variables.tf in this folder  
 
     
 }
