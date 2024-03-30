a2f2d6a3db6b4b9b0266e4b7e00c5eb6a6b78dc2
sagari <qhamsa05@gmail.com>
2024-03-30 23:11:09 +0530

    testing parameters

--- testing/main.tf
+++ testing/main.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "x1" {
 }
 
 resource "aws_instance" "x2" {
-
+    depends_on = [ aws_instance.x1 ]
     ami           = "ami-05295b6e6c790593e"
     instance_type = "t2.micro"
     tags          = {
